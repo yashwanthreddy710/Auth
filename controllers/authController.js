f263388bd0c52abccb2ff3bb87dd047a3a47ba32
yashwanthreddy710 <yeshwanthreddy216@gmail.com>
2025-04-10 19:01:26 +0530

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,10 +29,14 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
 
     const token = jwt.sign({ user : {id: user._id, name: user.username} }, process.env.JWT_SECRET, {expiresIn: '1h'});
     res.json({ token });
@@ -41,3 +45,8 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+exports.getUsers = async(req, res) => {
+    const users = await User.find();
+    res.json(users)
+}
